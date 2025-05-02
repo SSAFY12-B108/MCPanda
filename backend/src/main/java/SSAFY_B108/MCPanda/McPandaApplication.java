6c2779bd29db3feba15228675472cd680401647b
LeeJaeUk <20160230@o.cnu.ac.kr>
2025-05-02 10:01:12 +0900

    Backend 폴더 추가

---
+++ backend/src/main/java/SSAFY_B108/MCPanda/McPandaApplication.java
@@ -0,0 +1,13 @@
+package SSAFY_B108.MCPanda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McPandaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(McPandaApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/SSAFY_B108/MCPanda/McPandaApplicationTests.java
@@ -0,0 +1,13 @@
+package SSAFY_B108.MCPanda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class McPandaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
