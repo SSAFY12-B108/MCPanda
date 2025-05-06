144d78c560a68457132dc9a1293c7e9ec5746b8d
Lee Jae Uk <ki00035@naver.com>
2025-05-07 06:21:59 +0900

    fix: 백엔드 설정 및 오류 수정 - swagger-annotations 버전 업데이트, 불필요한 ComponentScan 제거, MongoDB 연결 설정 추가

---
+++ backend/src/main/java/SSAFY_B108/config/SwaggerConfig.java
@@ -0,0 +1,29 @@
+package SSAFY_B108.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .components(new Components()
+                        .addSecuritySchemes("bearer-jwt", 
+                                new SecurityScheme()
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")
+                                        .in(SecurityScheme.In.HEADER)
+                                        .name("Authorization")))
+                .info(new Info()
+                        .title("B108 프로젝트 API")
+                        .description("SSAFY B108 프로젝트 API 문서")
+                        .version("1.0.0"));
+    }
+} 
