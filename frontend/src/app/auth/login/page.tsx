e77ba1de503a459b8dc8a2c480febe96304e43e4
hyejoony <jeonbok.joha@gmail.com>
2025-05-12 13:11:36 +0900

    [chore] tab space 4 -> 2로 통일(frontend)

--- frontend/src/app/auth/login/page.tsx
+++ frontend/src/app/auth/login/page.tsx
@@ -3,18 +3,18 @@ import Chatbot from "@/components/Layout/Chatbot"
 import Image from "next/image"
 
 function page() {
-    return (
-        <div>
-            <Header />
-            <div className="flex flex-col items-center mt-10">
-                <Image src="/loginCharacter.png" alt="loginCharacter" width={160} height={160} className="w-40 mb-8"/>
-                <div className="text-2xl font-bold text-center mb-6">MCP 사용법을 쉽게 알려드려요</div>
-                <Image src="/googleLogin.png" alt="googleLoginBttn" width={280} height={280} className="w-70 mb-2"/>
-                <Image src="/githubLogin.png" alt="githubLoginBttn" width={280} height={280} className="w-70"/>
-            </div>
-            <Chatbot/>
-        </div>
-    )
+  return (
+    <div>
+      <Header />
+      <div className="flex flex-col items-center mt-10">
+        <Image src="/loginCharacter.png" alt="loginCharacter" width={160} height={160} className="w-40 mb-8" />
+        <div className="text-2xl font-bold text-center mb-6">MCP 사용법을 쉽게 알려드려요</div>
+        <Image src="/googleLogin.png" alt="googleLoginBttn" width={280} height={280} className="w-70 mb-2" />
+        <Image src="/githubLogin.png" alt="githubLoginBttn" width={280} height={280} className="w-70" />
+      </div>
+      <Chatbot />
+    </div>
+  )
 }
 
 export default page
