f747e314398a34593c1918706d0420fa6de5c7b1
hyejoony <jeonbok.joha@gmail.com>
2025-05-19 13:37:09 +0900

    [fix] - 게시글 수정 페이지 data type 일치하기 - 메인페이지 각 게시글 클릭시 router push - 메인페이지 페이지네이션 기능 추가 - 메인페이지 전체 게시글 조회 단일 api 재활용 코드로 수정

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -6,15 +6,17 @@ import HomeClient from '@/components/Home/HomeClient';
 
 export default async function Home() {
   const queryClient = new QueryClient();
+  const defaultCategory = '';
+  const defaultPage = 1;
 
   await queryClient.prefetchQuery({
-    queryKey: ['mainArticles'],
-    queryFn: fetchMainArticles,
+    queryKey: ['mainArticles', defaultPage],
+    queryFn: () => fetchMainArticles(defaultCategory,defaultPage),
   });
 
   return (
     <HydrationBoundary state={dehydrate(queryClient)}>
-      <HomeClient /> 
+      <HomeClient defaultCategory={defaultCategory} defaultPage={defaultPage} />
     </HydrationBoundary>
   );
 }
--- frontend/src/components/Home/CombinationBox.tsx
+++ frontend/src/components/Home/CombinationBox.tsx
@@ -3,11 +3,12 @@ interface ContentCardProps {
     title: string;
     tags: string[];
     recommendCount: number;
+    onClick?: () => void; // 클릭 이벤트 핸들러 추가
 }
 
-const CombinationBox: React.FC<ContentCardProps> = ({ title, tags, recommendCount }) => {
+const CombinationBox: React.FC<ContentCardProps> = ({ title, tags, recommendCount, onClick }) => {
     return (
-        <div className=" w-[200px] rounded-xl border border-gray-200 shadow-sm p-3 bg-white">
+        <div className=" w-[200px] rounded-xl border border-gray-200 shadow-sm p-3 bg-white hover:shadow-md transition-shadow mb-3 cursor-pointer" onClick={onClick}>
             <h2 className="text-sm font-medium text-gray-900 mb-4">{title}</h2>
             {/* 태그 + 좋아요를 한 줄에 정렬 */}
             <div className="flex justify-between ">
